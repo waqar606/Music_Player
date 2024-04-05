59b2b1ca7b0b055ec936b347e5518e80c539ac3d
Huzaifa Inshal <huzaifainshal@gmail.com>
2024-04-06 01:44:47 +0500

    'gui-fixes'

--- src/app/main/[search]/page.jsx
+++ src/app/main/[search]/page.jsx
@@ -31,25 +31,25 @@ const Main = ({ params }) => {
   },[popup])
 
   function downloadAudio(id,digit){
-    const url = [`https://apidl.net/api/button/mp3?url=https://www.youtube.com/watch?v=${id}`,
-    `https://www.yt2mp3s.me/api/widget?url=https://www.youtube.com/watch?v=${id}`];
+    const url = [`https://www.yt2mp3s.me/api/widget?url=https://www.youtube.com/watch?v=${id}`,
+    `https://apidl.net/api/widget?url=https://www.youtube.com/watch?v=${id}`];
     setUrl(url[digit]);
     setPopup(true)
   }
   return (
     <>
-    <div className="">
-      <Form value={params.search}/>
-    </div>
     {popup ? 
-    <div className='absolute z-2 overlay h-full w-full flex items-center justify-center' onClick={() => setPopup(false)}>
-    <div class="overlayItem w-4/5 h-1/2 bg-white px-1">
+    <div className='absolute z-2 overlay w-full h-full flex items-center justify-center' onClick={() => setPopup(false)}>
+    <div class="overlayItem w-4/5 h-3/4 bg-white px-1">
       <p className='text-black mt-4 ms-3 me-3'>Choose any download option (192kpbs recommended)</p>
       <iframe src={url} frameborder="0" className=' w-full h-full'
     sandbox="allow-forms allow-modals allow-pointer-lock  allow-same-origin allow-scripts allow-top-navigation allow-downloads"></iframe>
     </div>
     </div>
      :''} 
+    <div className="">
+      <Form value={params.search}/>
+    </div>
 
     <div className='container mt-8 px-16 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-md
     lg:gap-8
@@ -59,7 +59,7 @@ const Main = ({ params }) => {
           <div key={index} className='card bg-newGrey flex flex-col justify-between p-4'>
             <div>
               <div className=''>
-                {/* <img src={element.thumbnails[1].url} alt="" className='rounded-md w-full'/> */}
+                <img src={element.thumbnails[1].url} alt="Image Preview Unavailable" className='rounded-md w-full text-white'/>
               </div>
               <div>
                 <h1 className='text-white font-extrabold '>{element.name}</h1>
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -6,7 +6,7 @@ const Landing = () => {
             <div className="container mx-auto py-28">
                 <div className='text-center space-y-3'>
                     <h1 className="text-white text-6xl font-extrabold">Listen to</h1>
-                    <h1 className="um-gradient-text text-6xl font-extrabold">OR Download YT MP3s</h1>
+                    <h1 className="um-gradient-text text-6xl font-extrabold">OR Download YT MP3s or MP4s</h1>
                     <h1 className="text-white text-6xl font-extrabold">In Seconds</h1>
                     <h5 className="text-myGrey text-2xl">Give it a try right below, without having to sign up!</h5>
                 </div>
