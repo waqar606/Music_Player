7c894a0da90e826655e61f176a3a0e9f0d786fae
Huzaifa Inshal <huzaifainshal@gmail.com>
2024-04-06 01:54:49 +0500

    'guiFixes'

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -5,9 +5,9 @@ const Landing = () => {
         <section>
             <div className="container mx-auto py-28">
                 <div className='text-center space-y-3'>
-                    <h1 className="text-white text-2xl md:text-6xl font-extrabold">Listen to</h1>
-                    <h1 className="um-gradient-text text-2xl md:text-6xl font-extrabold">OR Download YT MP3s or MP4s</h1>
-                    <h1 className="text-white text-2xl md:text-6xl font-extrabold">In Seconds</h1>
+                    <h1 className="text-white text-3xl md:text-6xl font-extrabold">Listen to</h1>
+                    <h1 className="um-gradient-text text-3xl md:text-6xl font-extrabold">OR Download YT MP3s or MP4s</h1>
+                    <h1 className="text-white text-3xl md:text-6xl font-extrabold">In Seconds</h1>
                     <h5 className="text-myGrey md:text-2xl">Give it a try right below, without having to sign up!</h5>
                 </div>
                 <Form value={""}/>
