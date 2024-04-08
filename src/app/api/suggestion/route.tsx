7eda6109ad5dc8db07e6d88205694526777dd1ba
Huzaifa Inshal <huzaifainshal@gmail.com>
2024-04-09 01:44:29 +0500

    'updatedSearchOperation'

---
+++ src/app/api/suggestion/route.tsx
@@ -0,0 +1,21 @@
+// URL ENDPOINT http://localhost:3000/api/suggestion
+
+export async function GET(request:Request){
+    if (request.method === 'GET') {
+        const res = await fetch(
+            `https://clients1.google.com/complete/search?client=youtube&gs_ri=youtube&ds=yt&q=faded`,
+          );
+        const str = await res.text();
+        const arr = JSON.parse(str.substring(str.indexOf("["), str.indexOf("])") + 1));
+        let suggestionsTuple: [string, number, number[]][] = [];
+        if (Array.isArray(arr) && Array.isArray(arr.at(1))) {
+            suggestionsTuple = arr.at(1);
+        }
+        const suggestions = suggestionsTuple.flatMap((suggestion) => suggestion).filter((suggestion) => typeof suggestion === "string");
+        const suggestionss = String(suggestions);
+        const suggestedArray = suggestionss.split(",")
+        return new Response(JSON.stringify({"data":suggestedArray}));
+      } else {
+        return new Response('Method Not Allowed', { status: 405 });
+      }
+}
--- src/app/main/[search]/page.jsx
+++ src/app/main/[search]/page.jsx
@@ -1,17 +1,23 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Form from '@/components/Form'
 
 const Main = ({ params }) => {
   const [dataG, setData] = useState();
   const [url, setUrl] = useState('');
   const [popup, setPopup] = useState(false);
+  const iRef = useRef()
   useEffect(() => {
     const fetchResult = async () => {
-      const data = await fetch(`/api/search?q=${params.search}`);
+      //USING API (THAT USES YTMUSICAPI LIBRARY) BUILT WITHIN NEXTJS 
+      // const data = await fetch(`/api/search?q=${params.search}`);
+      // USING YMUSIC LIBRARY
+      const data = await fetch(`https://ymusic.cyclic.app/search?name=${params.search}`);
       const result = await data.json();
-      setData(result);
+      if(result.status=='success'){
+        setData(result.data);
+      }
     }
     fetchResult()
   }, [])
@@ -28,8 +34,10 @@ const Main = ({ params }) => {
   }, [popup])
 
   function downloadAudio(id, digit) {
-    const url = [`https://www.yt2mp3s.me/api/widget?url=https://www.youtube.com/watch?v=${id}`,
-    `https://apidl.net/api/widget?url=https://www.youtube.com/watch?v=${id}`];
+    const url = [
+      `https://apidl.net/api/widget?url=https://www.youtube.com/watch?v=${id}`,
+      `https://www.yt2mp3s.me/api/widget?url=https://www.youtube.com/watch?v=${id}`,
+  ];
     setUrl(url[digit]);
     setPopup(true);
     const p =document.createElement('p');
@@ -45,16 +53,18 @@ const Main = ({ params }) => {
       overContain.append(p)
       overContain.append(iframe)
     }
-    iframeWindow = iframe.contentWindow;
-    iframeWindow.console.log = function() { /* nop */ };
+    setTimeout(() => {
+      console.log(iframe.contentWindow)
+    }, 5000);
   }
+
   return (
     <>
       {popup ?
         <div className='absolute z-2 overlay w-full h-full flex items-center justify-center' onClick={() => setPopup(false)}>
           <div class="overlayItem w-4/5 h-3/4 bg-white px-1" id='overlay'>
             <p className='text-black mt-4 ms-3 me-3'>Choose any download option (192kpbs recommended)</p>
-            <iframe src={url} frameborder="0" className=' w-full h-full'
+            <iframe src={url} frameborder="0" className=' w-full h-full' ref={iRef}
               sandbox="allow-forms allow-modals allow-pointer-lock  allow-same-origin allow-scripts allow-top-navigation allow-downloads"></iframe>
           </div>
         </div>
@@ -68,14 +78,14 @@ const Main = ({ params }) => {
     '>
         {dataG ?
           dataG.map((element, index) => (
-            <div key={index} className='card bg-newGrey flex flex-col justify-between p-4'>
+            <div key={element.videoId} className='card bg-newGrey flex flex-col justify-between p-4'>
               <div>
                 <div className=''>
-                  <img src={element.thumbnails[1].url} alt="Image Preview Unavailable" className='rounded-md w-full text-white' />
+                  <img src={element.thumbnail} alt="Image Preview Unavailable" className='rounded-md w-full text-white' />
                 </div>
                 <div>
-                  <h1 className='text-white font-extrabold '>{element.name}</h1>
-                  <p className="text-myGrey">{element.artist.name}</p>
+                    <h1 className='text-white font-extrabold titleH1'>{element.title}</h1>
+                  <p className="text-myGrey">{element.channelTitle}</p>
                 </div>
               </div>
               <div className='flex flex-col space-y-2'>
