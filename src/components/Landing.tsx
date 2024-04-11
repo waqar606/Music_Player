8610029b4d991b23392d2025993ce05f594e716c
Huzaifa Inshal <huzaifainshal@gmail.com>
2024-04-11 18:13:19 +0500

    'improved-UIUX'

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -4,7 +4,7 @@ const Landing = () => {
     return (
         <section>
             <div className="container mx-auto py-28">
-                <div className='text-center space-y-3'>
+                <div className='text-center space-y-3 px-3  md:px-0'>
                     <h1 className="text-white text-3xl md:text-6xl font-extrabold">Listen to</h1>
                     <h1 className="um-gradient-text text-3xl md:text-6xl font-extrabold">OR Download YT MP3s or MP4s</h1>
                     <h1 className="text-white text-3xl md:text-6xl font-extrabold">In Seconds</h1>
