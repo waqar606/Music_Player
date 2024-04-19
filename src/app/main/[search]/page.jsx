dffc0d409e1af61106383f8756f2ea0ce8bc7702
Huzaifa Inshal <huzaifainshal@gmail.com>
2024-04-20 00:28:32 +0500

    'play'

--- src/app/main/[search]/page.jsx
+++ src/app/main/[search]/page.jsx
@@ -5,12 +5,12 @@ import Form from '@/components/Form'
 import Link from 'next/link'
 
 const Main = ({ params }) => {
-  const [showSugg,setShowSugg] = useState(false)
+  const [showSugg, setShowSugg] = useState(false)
   const [dataG, setData] = useState();
   const [url, setUrl] = useState('');
   const [popup, setPopup] = useState(false);
   const [popup2, setPopup2] = useState(false);
-  const [erro,setE] = useState(false)
+  const [erro, setE] = useState(false)
   useEffect(() => {
     const fetchResult = async () => {
       //USING API (THAT USES YTMUSICAPI LIBRARY) BUILT WITHIN NEXTJS 
@@ -18,9 +18,9 @@ const Main = ({ params }) => {
       // USING YMUSIC LIBRARY
       const data = await fetch(`https://ymusic.cyclic.app/search?name=${params.search}`);
       const result = await data.json();
-      if(result.status=='success'){
+      if (result.status == 'success') {
         setData(result.data);
-      }else if(result.status=='failed'){
+      } else if (result.status == 'failed') {
         setE(true)
       }
     }
@@ -42,25 +42,25 @@ const Main = ({ params }) => {
     const url = [
       `https://www.yt2mp3s.me/api/widget?url=https://www.youtube.com/watch?v=${id}`,
       `https://apidl.net/api/widget?url=https://www.youtube.com/watch?v=${id}`,
-  ];
+    ];
     setUrl(url[digit]);
     setPopup(true);
-    const p =document.createElement('p');
-    p.classList.add(['text-black','mt-4' ,'ms-3', 'me-3']);
+    const p = document.createElement('p');
+    p.classList.add(['text-black', 'mt-4', 'ms-3', 'me-3']);
     p.innerText = 'Choose any download option (192kpbs recommended)';
     const iframe = document.createElement('iframe');
     iframe.src = url;
     iframe.className = 'w-full h-full';
     iframe.sandbox = 'allow-forms allow-modals allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation allow-downloads';
     const overContain = document.getElementById('overlay');
-    if(overContain){
-      overContain.innerHTML='';
+    if (overContain) {
+      overContain.innerHTML = '';
       overContain.append(p)
       overContain.append(iframe)
     }
   }
 
-  async function downloadAudio(id,title) {
+  async function downloadAudio(id, title) {
     setPopup2(true);
     try {
       const response = await fetch(`/api/download?q=${id}`);
@@ -80,8 +80,8 @@ const Main = ({ params }) => {
     }
     setPopup2(false);
   }
-  
-  function handleClickFOrDropDown(){
+
+  function handleClickFOrDropDown() {
     setShowSugg(false)
   }
 
@@ -91,7 +91,7 @@ const Main = ({ params }) => {
         <div className='fixed z-2 overlay w-full h-full flex items-center justify-center' onClick={() => setPopup(false)}>
           <div className="overlayItem w-4/5 h-3/4 bg-white px-1" id='overlay'>
             <p className='text-black mt-4 ms-3 me-3'>Choose any download option (192kpbs recommended)</p>
-            <iframe src={url} frameborder="0" className=' w-full h-full' 
+            <iframe src={url} frameborder="0" className=' w-full h-full'
               sandbox="allow-forms allow-modals allow-pointer-lock  allow-same-origin allow-scripts allow-top-navigation allow-downloads"></iframe>
           </div>
         </div>
@@ -110,46 +110,44 @@ const Main = ({ params }) => {
       </div>
 
       {!erro ? <>
-      <div className='container mt-8 px-16 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-md
+        <div className='container mt-8 px-16 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-md
     lg:gap-8
     ' onClick={handleClickFOrDropDown}>
-        {dataG ?
-          dataG.map((element, index) => (
-            <div key={element.videoId} className='card bg-newGrey flex flex-col justify-between p-4'>
-              <div>
-                <div className=''>
-                  <img src={element.thumbnail} alt="Image Preview Unavailable" className='rounded-md w-full text-white' />
-                </div>
+          {dataG ?
+            dataG.map((element, index) => (
+              <div key={element.videoId} className='card bg-newGrey flex flex-col justify-between p-4'>
                 <div>
+                  <div className=''>
+                    <img src={element.thumbnail} alt="Image Preview Unavailable" className='rounded-md w-full text-white' />
+                  </div>
+                  <div>
                     <h1 className='text-white font-extrabold titleH1'>{element.title}</h1>
-                  <p className="text-myGrey">{element.channelTitle}</p>
+                    <p className="text-myGrey">{element.channelTitle}</p>
+                  </div>
                 </div>
-              </div>
-              <div className='flex flex-col space-y-2'>
-                <div>
-                  <button className='w-full p-3 border border-solid 
+                <div className='flex flex-col space-y-2'>
+                  <div>
+                    <button className='w-full p-3 border border-solid 
                     border-myPurple text-myPurple rounded font-bold hover:text-white hover:bg-myPurple'
-                    onClick={() => downloadAudio(element.videoId,element.title )}>Download</button>
-                </div>
-                <div className="flex items-center space-x-2">
-                  <a href={`/play/${element.videoId}`} target="_blank">
-                  <button className="text-white bg-black w-1/2 p-2 border border-solic 
+                      onClick={() => downloadAudio(element.videoId, element.title)}>Download</button>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <a href={`/play/${element.videoId}`} target="_blank" className="text-white bg-black w-1/2 p-2 border border-solic 
                     rounded hover:border-black hover:bg-white hover:text-black">
                       Play
-                    </button>
-                      </a>
-                  <button className="text-white bg-black w-1/2 p-2 border border-solic 
+                    </a>
+                    <button className="text-white bg-black w-1/2 p-2 border border-solic 
                     rounded hover:border-black hover:bg-white hover:text-black"
-                    onClick={() => moreDownloadAudio(element.videoId, 0)}>More</button>
+                      onClick={() => moreDownloadAudio(element.videoId, 0)}>More</button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))
-          :
-          <div className='text-white'>loading</div>
-        }
-      </div>
-        </>
+            ))
+            :
+            <div className='text-white'>loading</div>
+          }
+        </div>
+      </>
         : <div className='flex items-center justify-center text-white w-full mt-10'>Theres an issue with the app please try again later..</div>}
     </>
   );
