af3235553e9c1621c83daba10c97385c05703666
HuzaifaInshal <huzaifainshal@gmail.com>
2024-05-26 17:28:01 +0500

    localStorage work added

--- src/app/play/MusicPlayer.tsx
+++ src/app/play/MusicPlayer.tsx
@@ -1,10 +1,16 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay, faPause, faVolumeUp, faVolumeDown, faForward, faBackward } from '@fortawesome/free-solid-svg-icons';
-import '@/app/musicplayer.css'
-import MusicAnimation from '@/components/MusicAnimation';
-import MusicPlayAnima from '@/components/MusicPlayAnima';
-
+import {
+  faPlay,
+  faPause,
+  faVolumeUp,
+  faVolumeDown,
+  faForward,
+  faBackward,
+} from "@fortawesome/free-solid-svg-icons";
+import "@/app/musicplayer.css";
+import MusicAnimation from "@/components/MusicAnimation";
+import MusicPlayAnima from "@/components/MusicPlayAnima";
 
 interface MusicPlayerProps {
   audioURL: string;
@@ -13,15 +19,19 @@ interface MusicPlayerProps {
 const formatTime = (timeInSeconds: number): string => {
   const minutes = Math.floor(timeInSeconds / 60);
   const seconds = Math.floor(timeInSeconds % 60);
-  const formattedMinutes = String(minutes).padStart(2, '0');
-  const formattedSeconds = String(seconds).padStart(2, '0');
+  const formattedMinutes = String(minutes).padStart(2, "0");
+  const formattedSeconds = String(seconds).padStart(2, "0");
   return `${formattedMinutes}:${formattedSeconds}`;
 };
 
 const MusicPlayer: React.FC<MusicPlayerProps> = ({ audioURL }) => {
   const [isPlaying, setIsPlaying] = useState<boolean>(true);
   const [progress, setProgress] = useState<number>(0);
-  const [volume, setVolume] = useState<number>(100);
+  const [volume, setVolume] = useState<number>(
+    (Number(localStorage.getItem("volume")) == 0
+      ? 100
+      : Number(localStorage.getItem("volume"))) || 100
+  );
   const [currentTime, setCurrentTime] = useState<number>(0);
   const [duration, setDuration] = useState<number>(0);
   const audioRef = useRef<HTMLAudioElement>(null);
@@ -29,7 +39,6 @@ const MusicPlayer: React.FC<MusicPlayerProps> = ({ audioURL }) => {
   useEffect(() => {
     setIsPlaying(true);
   }, [audioURL]);
-
 
   const handlePlayPause = () => {
     if (isPlaying && audioRef.current) {
@@ -78,6 +87,7 @@ const MusicPlayer: React.FC<MusicPlayerProps> = ({ audioURL }) => {
     const volumeValue = parseFloat(e.target.value);
     if (audioRef.current) {
       audioRef.current.volume = volumeValue / 100;
+      localStorage.setItem("volume", String(volumeValue));
       setVolume(volumeValue);
     }
   };
@@ -97,12 +107,12 @@ const MusicPlayer: React.FC<MusicPlayerProps> = ({ audioURL }) => {
   return (
     <>
       {!audioURL ? (
-        <div className='empty'>Please search and select a song first to play</div>
+        <div className="empty">
+          Please search and select a song first to play
+        </div>
       ) : (
-        <div className='player-holder'>
-          {isPlaying ? (
-            <MusicPlayAnima />
-          ) : ('')}
+        <div className="player-holder">
+          {isPlaying ? <MusicPlayAnima /> : ""}
           <div id="container"></div>
           <audio
             autoPlay
@@ -110,25 +120,28 @@ const MusicPlayer: React.FC<MusicPlayerProps> = ({ audioURL }) => {
             src={audioURL}
             onTimeUpdate={handleTimeUpdate}
           />
-          <div className='player-controls'>
-            <div className='player-controllers-holder'>
-            <a onClick={backwardSong} className='player-controllers'>
+          <div className="player-controls">
+            <div className="player-controllers-holder">
+              <a onClick={backwardSong} className="player-controllers">
                 <FontAwesomeIcon icon={faBackward} />
               </a>
-              <a onClick={handlePlayPause} className='player-controllers player-controllers-1'>
+              <a
+                onClick={handlePlayPause}
+                className="player-controllers player-controllers-1"
+              >
                 {isPlaying ? (
                   <FontAwesomeIcon icon={faPause} />
                 ) : (
                   <FontAwesomeIcon icon={faPlay} />
                 )}
               </a>
-              <a onClick={forwardSong} className='player-controllers '>
+              <a onClick={forwardSong} className="player-controllers ">
                 <FontAwesomeIcon icon={faForward} />
               </a>
             </div>
           </div>
-          <div className='player-controls'>
-            <a onClick={mute} className='player-controllers'>
+          <div className="player-controls">
+            <a onClick={mute} className="player-controllers">
               {volume > 20 ? (
                 <FontAwesomeIcon icon={faVolumeUp} />
               ) : (
@@ -136,25 +149,25 @@ const MusicPlayer: React.FC<MusicPlayerProps> = ({ audioURL }) => {
               )}
             </a>
             <input
-              type='range'
-              className='volume'
+              type="range"
+              className="volume"
               min={0}
               max={100}
               value={volume}
               onChange={handleVolumeChange}
             />
           </div>
-          <div className='jojo'>
-            <span className='current-time'>{formatTime(currentTime)}</span>
+          <div className="jojo">
+            <span className="current-time">{formatTime(currentTime)}</span>
             <input
-              type='range'
-              className='duration'
+              type="range"
+              className="duration"
               min={0}
               max={100}
               value={progress}
               onChange={handleProgressBarChange}
             />
-            <span className='current-time'>{formatTime(duration)}</span>
+            <span className="current-time">{formatTime(duration)}</span>
           </div>
         </div>
       )}
